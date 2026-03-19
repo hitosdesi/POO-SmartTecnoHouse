68dd1bfa1a2fb8b1ac5e714e2b6d9f99cc3eabb7
Desiderio Hitos Padial <desiderio@email.com>
2026-03-19 19:40:50 +0100

    feat: añadido sensor humedad y persiana

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,22 +9,27 @@ public class Main {
         SensorTemperatura temp = new SensorTemperatura();
         SensorLuz luz = new SensorLuz();
         SensorPresencia presencia = new SensorPresencia();
+        SensorHumedad humedad = new SensorHumedad();
 
         Bombilla bombilla = new Bombilla();
         Ventilador ventilador = new Ventilador();
+        Persiana persiana = new Persiana();
 
         casa.agregarSensor(temp);
         casa.agregarSensor(luz);
         casa.agregarSensor(presencia);
+        casa.agregarSensor(humedad);
 
         casa.agregarActuador(bombilla);
         casa.agregarActuador(ventilador);
+        casa.agregarActuador(persiana);
 
         casa.actualizarSensores();
         casa.mostrarEstado();
 
         bombilla.ejecutarAccion("ON");
         ventilador.ejecutarAccion("HIGH");
+        persiana.ejecutarAccion("ABIERTA");
 
         System.out.println();
         System.out.println("Despues de activar actuadores:");
